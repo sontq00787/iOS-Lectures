5767c5f6dcb766fbef0d534a0992bcf113610a04
huongltt <sysvietnamcoltd03@HuongLTT-SY-4.local>
2015-01-13 15:16:15 +0700

    Custom TableView cell Full source

--- SimpleTable/SimpleTable/ViewController.m
+++ SimpleTable/SimpleTable/ViewController.m
@@ -14,18 +14,24 @@
 
 @implementation ViewController{
     NSArray *tableData;
+    NSArray *mobilePrices;
+    NSArray *mobileImages;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Initialize table data
     tableData = [NSArray arrayWithObjects:@"iPhone 6 Plus", @"Samsung Galaxy S4", @"Sony Xperia Z", @"iPad Air", @"Macbook air 2013", nil];
-    
+    mobilePrices = [NSArray arrayWithObjects:@"$299",@"$350",@"$350",@"$499",@"$899",nil];
+    mobileImages = [NSArray arrayWithObjects:@"ip6plus.jpg",@"s4.png",@"xperiaz.png",@"ipadAir2.jpg",@"macbookair.jpg", nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 60;
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
@@ -33,17 +39,36 @@
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    static NSString *tableCellIdentifier = @"SimpleTableCell";
+    static NSString *tableCellIdentifier = @"Cell";
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:tableCellIdentifier];
     
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:tableCellIdentifier];
     }
+    //hiển thị images
+    UIImageView *image = (UIImageView *)[cell.contentView viewWithTag:10];
+    image.image = [UIImage imageNamed:[mobileImages objectAtIndex:indexPath.row]];
+//    cell.imageView.image = [UIImage imageNamed:[mobileImages objectAtIndex:indexPath.row]];
     
-    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
-    cell.imageView.image = [UIImage imageNamed:@"codef.png"];
+    //hiển thị tên items
+    UILabel *labelItem = (UILabel *)[cell.contentView viewWithTag:11];
+    labelItem.text = [tableData objectAtIndex:indexPath.row];
+    
+    //hiển thị giá của items
+    UILabel *labelPrice = (UILabel*)[cell.contentView viewWithTag:12];
+    labelPrice.text = [mobilePrices objectAtIndex:indexPath.row];
+    
     return cell;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UIAlertView *messageAlert = [[UIAlertView alloc]
+                                 initWithTitle:[tableData objectAtIndex:indexPath.row] message:[mobilePrices objectAtIndex:indexPath.row] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    
+    // Display Alert Message
+    [messageAlert show];
+    
 }
 
 @end
