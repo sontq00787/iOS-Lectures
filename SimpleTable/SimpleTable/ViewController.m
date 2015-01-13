5f12443e9b240ec7a1743e58f2b41afc80e3c5cc
sontq <SonTQ@SonTQ.local>
2015-01-13 09:38:20 +0700

    Update Full source bài 2 part 1

--- SimpleTable/SimpleTable/ViewController.m
+++ SimpleTable/SimpleTable/ViewController.m
@@ -12,16 +12,37 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSArray *tableData;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Initialize table data
+    tableData = [NSArray arrayWithObjects:@"iPhone 6 Plus", @"Samsung Galaxy S4", @"Sony Xperia Z", @"iPad Air", @"Macbook air 2013", nil];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [tableData count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *tableCellIdentifier = @"SimpleTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:tableCellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:tableCellIdentifier];
+    }
+    
+    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    return cell;
 }
 
 @end
