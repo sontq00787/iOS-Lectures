40ecc9f6d3dedb4f7e2a4801e85008a651072c28
sontq <SonTQ@SonTQ.local>
2015-01-19 16:01:22 +0700

    Full source bài 5

--- SimpleTableWithSearchBar/SimpleTableWithSearchBar/ViewController.m
+++ SimpleTableWithSearchBar/SimpleTableWithSearchBar/ViewController.m
@@ -13,13 +13,14 @@
 @end
 
 @implementation ViewController{
-    NSArray *tableData;
+    NSMutableArray *tableData;
+    NSMutableArray *searchResults;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    tableData = [NSArray arrayWithObjects:@"Iphone 6", @"Samsung galaxy S5", @"Macbook Air Mid 2014", @"Apple Iphone 6 Plus", @"Sony Z", @"Google Nexus", nil];
+    tableData = [NSMutableArray arrayWithObjects:@"Iphone 6", @"Samsung galaxy S5", @"Macbook Air Mid 2014", @"Apple Iphone 6 Plus", @"Sony Z", @"Google Nexus", nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -28,6 +29,10 @@
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    //Trả về số rows tương ứng với kết quả tìm kiếm khi đang tìm kiếm
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        return searchResults.count;
+    }
     return [tableData count];
 }
 
@@ -36,7 +41,41 @@
     if(!cell){
         cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"SimpleTableCell"];
     }
-    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    //Sử dụng kết quả tìm kiếm để hiển thị ra các rows
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        cell.textLabel.text = [searchResults objectAtIndex:indexPath.row];
+    }
+    else{
+        cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    }
     return cell;
 }
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    //Xoá row đã chọn khỏi mảng
+    [tableData removeObjectAtIndex:indexPath.row];
+    // tải lại tablview
+    [tableView reloadData];
+
+}
+
+#pragma mark - UISearchDisplayController delegate methods
+-(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+{
+    [self filterContentForSearchText:searchString];
+    return YES;
+}
+
+#pragma mark - Content Filtering
+- (void)filterContentForSearchText:(NSString*)searchText
+{
+    
+    NSPredicate *resultPredicate = [NSPredicate
+                                    predicateWithFormat:@"SELF contains[c] %@",
+                                    searchText];
+    NSArray *tempArr = [tableData filteredArrayUsingPredicate:resultPredicate];
+    searchResults = [NSMutableArray arrayWithArray:tempArr];
+    
+}
+
 @end
